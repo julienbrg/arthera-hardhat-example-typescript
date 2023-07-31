d52bd6090d37ff11b0185d6591e20c5db11d8b6b
julienbrg <julien@strat.cc>
2023-07-31 09:25:35 +0200

    add subs

---
+++ contracts/Keita.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "./ISubscriptionOwner.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+
+/// @custom:security-contact julien@strat.cc
+contract Keita is ERC165, Ownable {
+    /// @notice Total number of talent contests won by Keita
+    uint public wins;
+
+    event Danced(uint indexed wins);
+
+    constructor() {}
+
+    /// @notice Inrements `dances`
+    function dance() public {
+        wins += 1;
+        emit Danced(wins);
+    }
+
+    function getSubscriptionOwner() external view returns (address) {
+        return owner();
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC165) returns (bool) {
+        return
+            interfaceId == type(ISubscriptionOwner).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+}
